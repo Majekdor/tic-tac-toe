27e7d61115e0a10005f5fa5826621732c1b5b7d1
Kevin Barnes <kevin@donniebarnes.com>
2022-11-03 13:06:27 -0400

    Initial Commit

---
+++ AppDevClubTicTacToe/AppDevClubTicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDevClubTicTacToeApp.swift
+//  AppDevClubTicTacToe
+//
+//  Created by Kevin Barnes on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct AppDevClubTicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppDevClubTicTacToe/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AppDevClubTicTacToe
+//
+//  Created by Kevin Barnes on 11/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
